24ad5138df6d755f835534a551fdee2a6e3da463
Luciano dos Santos <luciano.satiro1171@gmail.com>
2024-07-27 20:52:25 -0300

    default comment

--- web/script.sh
+++ web/script.sh
@@ -3,7 +3,7 @@
 # author:      luciano dos santos
 # contact:     https://www.linkedin.com/in/luciano-dos-santos-satiro
 # created:     09/03/2024
-# revision:    25/07/2024
+# revision:    27/07/2024
 # rule 1:      sem acento
 # rule 2:      variaveis em maiusculo
 # rule 3:      funcoes em maiusculo
@@ -26,24 +26,25 @@ IF_ELSE() {
 }
 
 # operacoes:
-sudo sed -i 's/deb\ http:\/\/archive.ubuntu.com\/ubuntu/deb\ http:\/\/br.archive.ubuntu.com\/ubuntu/g' /etc/apt/sources.list
+# altera o repositorio:
+sed -i 's/deb\ http:\/\/archive.ubuntu.com\/ubuntu/deb\ http:\/\/br.archive.ubuntu.com\/ubuntu/g' /etc/apt/sources.list
 	IF_ELSE "sources.list alterado" "sources.list nao alterado"
 
 # atualiza repositorios e pacotes:
-sudo apt-get -y update
+apt-get -y update
 	IF_ELSE "em update" "em update"
 
-sudo apt-get -y upgrade
+apt-get -y upgrade
 	IF_ELSE "em upgrade" "em upgrade"
 
 # instala pacotes:
 for PACOTE in $LISTA; do
-        sudo apt-get -y install $PACOTE
+        apt-get -y install $PACOTE
                 IF_ELSE "$PACOTE foi instalado" "$PACOTE nao foi instalado"
 done
 
 # altera o idioma:
-sudo locale-gen pt_BR.UTF-8
+locale-gen pt_BR.UTF-8
 	IF_ELSE "idioma alterado" "idioma nao alterado"
 
 # altera o timezone:
